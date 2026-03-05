84ae8f9c71a4e72d8338590c00b1d2146d46704b
BernardoAlfredochinchinchinchin <bvandepoll05@gmail.com>
2026-03-05 23:18:51 +0100

    Homepage
    kleine landing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h1 class="text-3xl font-bold mb-4">Welkom bij BM Performance</h1>
+                    <p class="text-lg mb-2">Uw specialist in auto onderhoud en reparatie.</p>
+                    <p>U bent succesvol ingelogd. Gebruik het menu om verder te gaan.</p>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
